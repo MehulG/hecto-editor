27c6f9973d0c35a31dfbb1342f74551a77bc83a9
mehulg <mehulgaidhani@gmail.com>
2023-01-05 21:15:06 +0530

    status bar

--- src/document.rs
+++ src/document.rs
@@ -2,21 +2,25 @@ use crate::Row;
 use std::fs;
 
 #[derive(Default)]
-pub struct Document{
-    rows : Vec<Row>
+pub struct Document {
+    rows: Vec<Row>,
+    pub file_name: Option<String>,
 }
 
 impl Document {
-    pub fn open(filename: &str) -> Result<Self, std::io::Error>{
+    pub fn open(filename: &str) -> Result<Self, std::io::Error> {
         let contents = fs::read_to_string(filename)?;
         let mut rows = Vec::new();
         for val in contents.lines() {
             rows.push(Row::from(val));
         }
-        Ok(Self{rows})
+        Ok(Self {
+            rows,
+            file_name: Some(filename.to_string()),
+        })
     }
 
-    pub fn row(&self, index: usize) -> Option<&Row>{
+    pub fn row(&self, index: usize) -> Option<&Row> {
         self.rows.get(index)
     }
 
@@ -24,7 +28,7 @@ impl Document {
         self.rows.is_empty()
     }
 
-    pub fn len(&self) -> usize{
+    pub fn len(&self) -> usize {
         self.rows.len()
     }
 }
